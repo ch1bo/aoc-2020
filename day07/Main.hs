83bd7c5ad889effbf0b6e48f25cc3dc0129c6560
Sebastian Nagel <sebastian.nagel@ncoding.at>
2020-12-08 20:09:16 +0100

    Refactor broken Tree -> Bag

--- day07/Main.hs
+++ day07/Main.hs
@@ -1,6 +1,5 @@
 module Main where
 
-import Text.Printf (printf)
 import Data.List.Extra (splitOn, breakOn)
 import Data.Map (Map)
 import qualified Data.Map as Map
@@ -24,32 +23,25 @@ parseInput s = foldMap parseLine $ lines s
     let (numString, rest) = breakOn " " b
     in (read numString, head . splitOn " bag" $ tail rest)
 
-data Tree = Leaf String
-          | Tree String [(Int, Tree)]
+data Bag = Bag { name :: String, contents :: [(Int, Bag)] }
           deriving Show
 
-rootName :: Tree -> String
-rootName (Leaf s) = s
-rootName (Tree s _) = s
-
-toForest :: Input -> [Tree]
-toForest m = map toTree $ Map.keys m
+toBags :: Input -> [Bag]
+toBags m = map toBag $ Map.keys m
  where
-  toTree :: String -> Tree
-  toTree s = case Map.lookup s m of
-    Nothing -> Leaf s
-    Just [] -> Leaf s
-    Just cs -> Tree s (map (second toTree) cs)
+  toBag :: String -> Bag
+  toBag s = Bag
+    { name = s
+    , contents = maybe [] (map (second toBag)) $ Map.lookup s m
+    }
 
-containsBag :: String -> Tree -> Bool
-containsBag s (Leaf n) = s == n
-containsBag s (Tree n cs)
+containsBag :: String -> Bag -> Bool
+containsBag s (Bag n cs)
   | s == n = True
   | otherwise = any (containsBag s . snd) cs
 
-countBagsIn :: Tree -> Int
-countBagsIn (Leaf _) = 0
-countBagsIn (Tree _ cs) = foldr go 0 cs
+countBagsIn :: Bag -> Int
+countBagsIn (Bag _ cs) = foldr go 0 cs
   where go (i, t) s = s + i * (countBagsIn t + 1)
 
 part1 :: Input -> String
@@ -58,19 +50,19 @@ part1 =
     . pred -- -1 because "shiny gold" is also in the result
     . length
     . filter (containsBag "shiny gold")
-    . toForest
+    . toBags
 
 part2 :: Input -> String
 part2 =
   show
     . maybe (error "missing a bag!") countBagsIn
-    . findTree "shiny gold"
-    . toForest
+    . findBag "shiny gold"
+    . toBags
  where
-  findTree _ [] = Nothing
-  findTree s (t : ts)
-    | rootName t == s = Just t
-    | otherwise = findTree s ts
+  findBag _ [] = Nothing
+  findBag s (t : ts)
+    | name t == s = Just t
+    | otherwise = findBag s ts
 
 main :: IO ()
 main = do
