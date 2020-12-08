5d695cfe8b46d9cd73dc8d26b47811a04cb0a8f1
Sebastian Nagel <sebastian.nagel@ncoding.at>
2020-12-08 20:04:42 +0100

    Solved day07 part2 ..still with a (christmas) tree

--- day07/Main.hs
+++ day07/Main.hs
@@ -5,6 +5,7 @@ import Data.List.Extra (splitOn, breakOn)
 import Data.Map (Map)
 import qualified Data.Map as Map
 import Data.Bifunctor (Bifunctor(second))
+import GHC.IO (unsafePerformIO)
 
 type Input = Map String [(Int, String)]
 
@@ -27,19 +28,29 @@ data Tree = Leaf String
           | Tree String [(Int, Tree)]
           deriving Show
 
+rootName :: Tree -> String
+rootName (Leaf s) = s
+rootName (Tree s _) = s
+
 toForest :: Input -> [Tree]
 toForest m = map toTree $ Map.keys m
  where
   toTree :: String -> Tree
   toTree s = case Map.lookup s m of
     Nothing -> Leaf s
+    Just [] -> Leaf s
     Just cs -> Tree s (map (second toTree) cs)
 
 containsBag :: String -> Tree -> Bool
-containsBag s (Leaf n) | s == n = True
+containsBag s (Leaf n) = s == n
 containsBag s (Tree n cs)
   | s == n = True
   | otherwise = any (containsBag s . snd) cs
+
+countBagsIn :: Tree -> Int
+countBagsIn (Leaf _) = 0
+countBagsIn (Tree _ cs) = foldr go 0 cs
+  where go (i, t) s = s + i * (countBagsIn t + 1)
 
 part1 :: Input -> String
 part1 =
@@ -48,19 +59,34 @@ part1 =
     . length
     . filter (containsBag "shiny gold")
     . toForest
-  -- unlines . map (\t -> show (containsBag "shiny gold" t, t)) . toForest
 
 part2 :: Input -> String
-part2 = undefined
+part2 =
+  show
+    . maybe (error "missing a bag!") countBagsIn
+    . findTree "shiny gold"
+    . toForest
+ where
+  findTree _ [] = Nothing
+  findTree s (t : ts)
+    | rootName t == s = Just t
+    | otherwise = findTree s ts
 
 main :: IO ()
 main = do
-  go "test"
-  go "input"
- where
-  go fn = do
-    testInput <- parseInput <$> readFile fn
-    printf "Part one (%s):\n" fn
-    putStrLn $ part1 testInput
-    -- printf "Part two (%s):\n" fn
-    -- print $ part2 testInput
+  putStrLn "Part one (test):"
+  putStrLn $ part1 test
+  putStrLn "Part two (test):"
+  putStrLn $ part2 test
+  putStrLn "Part one (input):"
+  putStrLn $ part1 input
+  putStrLn "Part two (input):"
+  putStrLn $ part2 input
+
+test :: Input
+test = unsafePerformIO $ parseInput <$> readFile "test"
+{-# NOINLINE test #-}
+
+input :: Input
+input = unsafePerformIO $ parseInput <$> readFile "input"
+{-# NOINLINE input #-}
