47a5f61ec24c30aff82dcfd75360aae5d528f89b
Sebastian Nagel <sebastian.nagel@ncoding.at>
2020-12-15 18:49:01 +0100

    Solve day13 part1

--- day13/Main.hs
+++ day13/Main.hs
@@ -3,14 +3,30 @@
 module Main where
 
 import System.IO.Unsafe (unsafePerformIO)
+import Data.List.Extra (splitOn)
+import Data.List (sort)
+import Data.Foldable (minimumBy)
+import Data.Function (on)
 
-type Input = String
+type Input = (Time, [Bus])
+
+type Time = Int
+type Bus = Int
 
 parseInput :: String -> Input
-parseInput = id
+parseInput s =
+  let
+    (t : x : _) = splitOn "\n" s
+    bs = filter (/= "x") $ splitOn "," x
+  in (read t, map read bs)
 
 part1 :: Input -> String
-part1 = undefined
+part1 (t, bs) =
+  let
+    departures = map (\b -> (t `div` b + 1) * b) bs
+    (nextTime, nextBus) = minimumBy (compare `on` fst) $ zip departures bs
+    wait = nextTime - t
+  in show (nextBus * wait)
 
 part2 :: Input -> String
 part2 = undefined
@@ -21,10 +37,10 @@ main = do
   putStrLn $ part1 test
   putStrLn "Part one (input):"
   putStrLn $ part1 input
-  putStrLn "Part two (test):"
-  putStrLn $ part2 test
-  putStrLn "Part two (input):"
-  putStrLn $ part2 input
+  -- putStrLn "Part two (test):"
+  -- putStrLn $ part2 test
+  -- putStrLn "Part two (input):"
+  -- putStrLn $ part2 input
 
 test :: Input
 test = unsafePerformIO $ parseInput <$> readFile "test"
