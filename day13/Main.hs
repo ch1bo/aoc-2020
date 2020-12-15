3e7833f354fd30178d59a6c700b39d11064f0653
Sebastian Nagel <sebastian.nagel@ncoding.at>
2020-12-15 20:37:38 +0100

    Solve day13 part2

--- day13/Main.hs
+++ day13/Main.hs
@@ -1,35 +1,45 @@
 {-# LANGUAGE FlexibleContexts #-}
 {-# LANGUAGE LambdaCase #-}
+{-# LANGUAGE BangPatterns #-}
 module Main where
 
 import System.IO.Unsafe (unsafePerformIO)
 import Data.List.Extra (splitOn)
-import Data.List (sort)
 import Data.Foldable (minimumBy)
 import Data.Function (on)
+import Data.Tuple.Extra (first)
+import Debug.Trace (trace)
+import Text.Printf (printf)
 
 type Input = (Time, [Bus])
 
-type Time = Int
-type Bus = Int
+type Time = Integer
+
+type Bus = (Integer, Integer) -- (id, index)
 
 parseInput :: String -> Input
 parseInput s =
   let
     (t : x : _) = splitOn "\n" s
-    bs = filter (/= "x") $ splitOn "," x
-  in (read t, map read bs)
+    bs = filter ((/= "x") . fst) $ zip (splitOn "," x) [0 ..]
+  in (read t, map (first read) bs)
 
 part1 :: Input -> String
 part1 (t, bs) =
   let
-    departures = map (\b -> (t `div` b + 1) * b) bs
-    (nextTime, nextBus) = minimumBy (compare `on` fst) $ zip departures bs
+    bs'  = map fst bs
+    departures = map (\b -> (t `div` b + 1) * b) bs'
+    (nextTime, nextBus) = minimumBy (compare `on` fst) $ zip departures bs'
     wait = nextTime - t
   in show (nextBus * wait)
 
 part2 :: Input -> String
-part2 = undefined
+part2 (_, bs) = show $ go 1 1 bs
+ where
+  go !i _ [] = i
+  go !i inc ((bid, idx) : next)
+    | (i + idx) `mod` bid == 0 = go i (lcm bid inc) next
+    | otherwise = go (i + inc) inc bs
 
 main :: IO ()
 main = do
@@ -37,10 +47,10 @@ main = do
   putStrLn $ part1 test
   putStrLn "Part one (input):"
   putStrLn $ part1 input
-  -- putStrLn "Part two (test):"
-  -- putStrLn $ part2 test
-  -- putStrLn "Part two (input):"
-  -- putStrLn $ part2 input
+  putStrLn "Part two (test):"
+  putStrLn $ part2 test
+  putStrLn "Part two (input):"
+  putStrLn $ part2 input
 
 test :: Input
 test = unsafePerformIO $ parseInput <$> readFile "test"
