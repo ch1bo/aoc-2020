f67426f39644620f80769fcd20a595d2e569148e
Sebastian Nagel <sebastian.nagel@ncoding.at>
2020-12-15 18:10:42 +0100

    Prepare day13

---
+++ day13/Main.hs
@@ -0,0 +1,35 @@
+{-# LANGUAGE FlexibleContexts #-}
+{-# LANGUAGE LambdaCase #-}
+module Main where
+
+import System.IO.Unsafe (unsafePerformIO)
+
+type Input = String
+
+parseInput :: String -> Input
+parseInput = id
+
+part1 :: Input -> String
+part1 = undefined
+
+part2 :: Input -> String
+part2 = undefined
+
+main :: IO ()
+main = do
+  putStrLn "Part one (test):"
+  putStrLn $ part1 test
+  putStrLn "Part one (input):"
+  putStrLn $ part1 input
+  putStrLn "Part two (test):"
+  putStrLn $ part2 test
+  putStrLn "Part two (input):"
+  putStrLn $ part2 input
+
+test :: Input
+test = unsafePerformIO $ parseInput <$> readFile "test"
+{-# NOINLINE test #-}
+
+input :: Input
+input = unsafePerformIO $ parseInput <$> readFile "input"
+{-# NOINLINE input #-}
