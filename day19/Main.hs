407267d1a1ae4f84aac378a2b263b7aac5676715
Sebastian Nagel <sebastian.nagel@ncoding.at>
2020-12-22 20:05:35 +0100

    Solve day19 part2 with an .. ambivalent parser

--- day19/Main.hs
+++ day19/Main.hs
@@ -1,3 +1,4 @@
+{-# LANGUAGE ScopedTypeVariables #-}
 {-# LANGUAGE TupleSections #-}
 {-# LANGUAGE RecordWildCards #-}
 {-# LANGUAGE FlexibleContexts #-}
@@ -5,12 +6,14 @@
 module Main where
 
 import System.IO.Unsafe (unsafePerformIO)
-import Data.IntMap ((!), (!?), IntMap)
-import Data.List.Extra (isPrefixOf, splitOn)
+import Data.IntMap ((!?), IntMap)
+import Data.List.Extra (splitOn)
 import qualified Data.IntMap as Map
 import Data.Either (rights)
-import Debug.Trace (trace)
-import Data.Tuple.Extra (first)
+import Control.Monad (join)
+import Data.List (nub)
+import Data.Foldable (find)
+import Data.Bifunctor (Bifunctor(first))
 
 data Input = Input { rules :: IntMap Rule
                    , messages :: [String]
@@ -22,68 +25,48 @@ data Rule = C Char
           deriving Show
 
 -- | A hand-rolled, very specific parser with an opinionated parser state.
+--
+-- For part2, we need to be able to return multiple matches/remaining strings
+-- which complicates .. a lot.
 newtype RuleP = RuleP
-  { runRuleParser :: IntMap Rule -> String -> Either (String, String) (String, String) }
+  { runRuleParser :: IntMap Rule -> String -> Either String [(String, String)] }
 
 ruleP :: Rule -> RuleP
 ruleP r = case r of
-  C c -> charP c
-  R a -> indexP a
-  R2 a b ->
-    let (prefix, a', b') = commonPrefix a b
-    in
-      combine (indexP prefix) $ if length a' >= length b'
-        then indexP a' `orElse` indexP b'
-        else indexP b' `orElse` indexP a'
-
-commonPrefix :: Eq a => [a] -> [a] -> ([a], [a], [a])
-commonPrefix [] b  = ([], [], b)
-commonPrefix a  [] = ([], a, [])
-commonPrefix (a : as) (b : bs)
-  | a == b    = let (p, x, y) = commonPrefix as bs in (a : p, x, y)
-  | otherwise = ([], a : as, b : bs)
+  C c    -> charP c
+  R a    -> indexP a
+  R2 a b -> indexP a `orElse` indexP b
 
 charP :: Char -> RuleP
 charP c = RuleP $ \_rs -> \case
   (x : xs)
-    | x == c -> pure ([c], xs)
-    | otherwise -> Left
-      ("charP expected " ++ show c ++ " but got " ++ show x, x : xs)
-  [] -> Left ("charP expected " ++ show c ++ " but got nothing", [])
+    | x == c    -> pure [([c], xs)]
+    | otherwise -> Left ("charP expected " ++ show c ++ " but got " ++ show x)
+  [] -> Left ("charP expected " ++ show c ++ " but got nothing")
 
 indexP :: [Int] -> RuleP
-indexP [] = RuleP $ \_ s -> pure ([], s)
+indexP [] = RuleP $ \_ s -> pure [([], s)]
 indexP (i : is) = RuleP $ \rs s -> case rs !? i of
-  Nothing -> Left ("no parse: indexP not found: " ++ show i, s)
-  Just r  -> runRuleParser
-    (combine (label (show i ++ ": " ++ show r) $ ruleP r) (indexP is))
-    rs
-    s
+  Nothing -> Left ("no parse: indexP not found: " ++ show i)
+  Just r  -> runRuleParser (combine (ruleP r) (indexP is)) rs s
 
 combine :: RuleP -> RuleP -> RuleP
 combine a b = RuleP $ \rs s -> do
-  (ra, resta) <- runRuleParser a rs s
-  (rb, restb) <- runRuleParser b rs resta
-  pure (ra ++ rb, restb)
+  ras <- runRuleParser a rs s
+  case rights $ map (runB rs) ras of
+    []  -> Left $ "combine: right-hand did not parse any of " ++ show ras
+    rbs -> pure $ join rbs
+ where
+  runB rs (ra, resta) = case runRuleParser b rs resta of
+    Left  l -> Left l
+    Right x -> Right $ map (first (ra ++)) x
 
 orElse :: RuleP -> RuleP -> RuleP
 orElse a b = RuleP $ \rs s -> case runRuleParser a rs s of
-  Right ([], rest) -> runRuleParser b rs rest
-  Right x -> pure x
-  Left  _ -> runRuleParser b rs s
-
-label :: String -> RuleP -> RuleP
-label l p = RuleP $ \rs s ->
-  case trace (l ++ " ? " ++ show s) runRuleParser p rs s of
-    Right (x, rest) ->
-      trace (l ++ " > " ++ show x ++ ", " ++ show rest) pure (x, rest)
-    Left (e, rest) -> trace (l ++ " X " ++ show rest) Left (l ++ e, rest)
-
-doneAfter :: RuleP -> RuleP
-doneAfter p = RuleP $ \rs s -> case runRuleParser p rs s of
-  Right (r, []) -> pure (r, [])
-  Right (_, rest) -> Left ("unconsumed", rest)
-  Left  x -> Left x
+  Left  _  -> runRuleParser b rs s
+  Right ra -> case runRuleParser b rs s of
+    Left  _  -> pure ra
+    Right rb -> pure $ nub $ ra ++ rb
 
 parseInput :: String -> Input
 parseInput s =
@@ -101,13 +84,27 @@ readRule s = let [i, rest] = splitOn ": " s in (read i, go rest)
 
   parse = map read . words
 
+doneAfter :: RuleP -> RuleP
+doneAfter p = RuleP $ \rs s -> case runRuleParser p rs s of
+  Right rs -> case find (null . snd) rs of
+    Nothing -> Left $ "unconsumed: " ++ show rs
+    Just x  -> pure [x]
+  Left x -> Left x
+
+parseOnly :: RuleP -> IntMap Rule -> String -> Either String String
+parseOnly p rs s = do
+  res <- runRuleParser p rs s
+  case find (null . snd) res of
+    Nothing     -> Left $ "unconsumed: " ++ show rs
+    Just (x, _) -> pure x
+
 part1 :: Input -> String
 part1 Input {..} = show . length . rights $ map parse messages
-  where parse = runRuleParser (doneAfter $ ruleP $ R [0]) rules
+  where parse = parseOnly (indexP [0]) rules
 
 part2 :: Input -> String
-part2 i = unlines . map show $ map parse $ messages i
-  where parse = runRuleParser (doneAfter $ ruleP $ R [0]) $ rules' i
+part2 i@Input {..} = show . length . rights $ map parse messages
+  where parse = parseOnly (indexP [0]) $ rules' i
 
 rules' :: Input -> IntMap Rule
 rules' =
@@ -117,14 +114,14 @@ rules' =
 
 main :: IO ()
 main = do
-  -- putStrLn "Part one (test):"
-  -- putStrLn $ part1 test
-  -- putStrLn "Part one (input):"
-  -- putStrLn $ part1 input
+  putStrLn "Part one (test):"
+  putStrLn $ part1 test
+  putStrLn "Part one (input):"
+  putStrLn $ part1 input
   putStrLn "Part two (test):"
   putStrLn $ part2 test
-  -- putStrLn "Part two (input):"
-  -- putStrLn $ part2 input
+  putStrLn "Part two (input):"
+  putStrLn $ part2 input
 
 test :: Input
 test = unsafePerformIO $ parseInput <$> readFile "test"
