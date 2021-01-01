891e3d46d8edb118a059ceb7cb8af35ec09e8d45
Sebastian Nagel <sebastian.nagel@ncoding.at>
2021-01-01 14:39:16 +0100

    Solve day22 part2

--- day22/Main.hs
+++ day22/Main.hs
@@ -3,13 +3,12 @@
 module Main where
 
 import System.IO.Unsafe (unsafePerformIO)
-import GHC.Natural (Natural)
 import Data.List.Extra (splitOn)
 import Prelude hiding (round)
 
 type Input = Game
 
-type Game = ([Natural], [Natural])
+type Game = ([Int], [Int])
 
 parseInput :: String -> Input
 parseInput s = (parse p1, parse p2)
@@ -26,20 +25,46 @@ round (c1 : p1, c2 : p2)
   | c1 < c2   = (p1, p2 ++ [c2, c1])
   | otherwise = error "undefined game state"
 
-type Score = Natural
+type Score = Int
 
-play :: Game -> Score
-play = \case
-  (p1, []) -> score p1
-  ([], p2) -> score p2
-  g -> play $ round g
-  where score cs = sum $ zipWith (*) (reverse cs) [1 ..]
+data Result = P1Wins [Int] | P2Wins [Int]
+
+resultScore :: Result -> Score
+resultScore (P1Wins cs) = score cs
+resultScore (P2Wins cs) = score cs
+
+score :: [Int] -> Int
+score cs = sum $ zipWith (*) (reverse cs) [1 ..]
+
+play1 :: Game -> Result
+play1 = \case
+  (p1, []) -> P1Wins p1
+  ([], p2) -> P2Wins p2
+  g -> play1 $ round g
 
 part1 :: Input -> String
-part1 = undefined
+part1 = show . resultScore . play1
+
+type History = [Game]
+
+play2 :: History -> Game -> Result
+play2 hs g@(p1, _)
+  | g `elem` hs = P1Wins p1
+  | otherwise = case g of
+    (p1, []) -> P1Wins p1
+    ([], p2) -> P2Wins p2
+    (c1 : p1, c2 : p2)
+      | length p1 >= c1 && length p2 >= c2
+      -> -- Play sub-game
+         case play2 hs (take c1 p1, take c2 p2) of
+        P1Wins _ -> play2 (g : hs) (p1 ++ [c1, c2], p2)
+        P2Wins _ -> play2 (g : hs) (p1, p2 ++ [c2, c1])
+      | otherwise
+      -> -- Play normal round
+         play2 (g : hs) $ round g
 
 part2 :: Input -> String
-part2 = undefined
+part2 = show . resultScore . play2 []
 
 main :: IO ()
 main = do
